68a76a89e05ac48e7d8dc1e0f5e881d02e541a43
Alan Daniels <alan.daniels@hotmail.com>
2016-09-10 14:09:26 -0400

    make titles collapsable in animated resume

--- app/assets/javascripts/interactive_resume.js
+++ app/assets/javascripts/interactive_resume.js
@@ -9,9 +9,49 @@ function bsod(){
 }
 
 function recover(){
+  debugger;
   document.getElementById('BSOD').style.visibility = "hidden";
   document.getElementById('one').style.visibility = "visible";
   document.getElementById('two').style.visibility = "visible";
   document.getElementById('three').style.visibility = "visible";
   document.getElementById('four').style.visibility = "visible";
+}
+
+function toggle(toggleElement, toggleTriangle){
+  if (toggleElement.style.display == 'none'){
+    toggleElement.style.display = 'inline';
+  }
+  else {
+    toggleElement.style.display = 'none';
+  }
+  if (toggleTriangle.innerHTML == String.fromCharCode(9660)){
+    toggleTriangle.innerHTML = '&#9650';
+  }
+  else {
+    toggleTriangle.innerHTML = '&#9660';
+  }
+}
+
+function toggle1(){
+  var toggleElement = document.getElementById('one-body');
+  var toggleTriangle = document.getElementById('toggle-triangle-one');
+  toggle(toggleElement, toggleTriangle);
+}
+
+function toggle2(){
+  var toggleElement = document.getElementById('two-body');
+  var toggleTriangle = document.getElementById('toggle-triangle-two');
+  toggle(toggleElement, toggleTriangle);
+}
+
+function toggle3(){
+  var toggleElement = document.getElementById('three-body');
+  var toggleTriangle = document.getElementById('toggle-triangle-three');
+  toggle(toggleElement, toggleTriangle);
+}
+
+function toggle4(){
+  var toggleElement = document.getElementById('four-body');
+  var toggleTriangle = document.getElementById('toggle-triangle-four');
+  toggle(toggleElement, toggleTriangle);
 }
